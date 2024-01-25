18139505423407bfccb66ce9648e2d11734af133
jujube1123 <152823522+jujube1123@users.noreply.github.com>
2024-01-25 09:18:06 +0900

    YangJs.js 에러 수정

--- src/main/webapp/resources/js/Yang/YangJs.js
+++ src/main/webapp/resources/js/Yang/YangJs.js
@@ -1,8 +1,8 @@
 //아이디 유효성 검사
 
 $(document).ready(function() {
-						//영문			//숫자
-	var RegexId = /^[a-z0-9_-]{3,16}$/,/[0-9]/;
+
+	var RegexId = /^[a-z0-9_-]{3,16}[0-9]$/;
 	$('#Name').blur(function() {
 		if($.trim($("#Name").val())==""){
 			$('.idchk').html("영문이름을 입력해주세요").css('color','red');
@@ -26,11 +26,10 @@ $(document).ready(function() {
 							$('#Name').val("");
 						} else if (data == "noredundancy") {
 							$('.idchk').html("사용가능한 아이디입니다").css('color', 'green');
-							;
 						}
 					}
 				});
 			}
 			
-	};
-};
+	});
+});
